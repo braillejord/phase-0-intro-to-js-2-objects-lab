74e261feff3babe1d7127b68155fc0e793aef4bd
BreElle Wells <breellejordyn@gmail.com>
2023-05-21 14:20:05 -0600

    create object and perform operations on it

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Frank",
+    streetAddress: "123 Eyes on Me",
+};
+
+function updateEmployeeWithKeyAndValue(Object, key, value) {
+    return {
+        ...Object,
+    [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeClone = {...employee};
+    delete employeeClone[key];
+    return employeeClone;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
